ad99477dfa53deddbb2b3f60fe6d82fb9b4ea99e
Kuba Trzykowski <J.A.Trzykowski@student.tudelft.nl>
2022-03-03 15:08:33 +0100

    Add javadocs for PlayerScore
    Added documentation for the PlayerScore class

--- commons/src/main/java/commons/PlayerScore.java
+++ commons/src/main/java/commons/PlayerScore.java
@@ -13,6 +13,10 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.sql.Timestamp;
 
+/**
+ * A class to store scores of players in the database
+ * An instance consists of a player name, the score obtained and the time it was achieved
+ */
 @Entity
 public class PlayerScore {
 
@@ -29,22 +33,43 @@ public class PlayerScore {
         // for object mapper
     }
 
+    /**
+     * Create a playerScore instance
+     * @param playerName - name of the player
+     * @param score - the score they scored in the game
+     * @param time - Timestamp when the scored was achieved
+     */
     public PlayerScore(String playerName, int score, Timestamp time) {
         this.playerName = playerName;
         this.score = score;
         this.time = time;
     }
 
+    /**
+     * Compare whether two instances of a PlayerScore are equal
+     * All fields have to be equal for equality
+     * @param obj - to be compared with
+     * @return - whether the two objects are equal
+     */
     @Override
     public boolean equals(Object obj) {
         return EqualsBuilder.reflectionEquals(this, obj);
     }
 
+    /**
+     * Get a hash code of the PlayerScore instance
+     * @return - the hash code
+     */
     @Override
     public int hashCode() {
         return HashCodeBuilder.reflectionHashCode(this);
     }
 
+    /**
+     * Get a string representation of the PlayerScore instance
+     * It is given in a multiline format
+     * @return - the string representation of PlayerScore
+     */
     @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);

95d1dd823e74d4c68b87601b47a758e78b1b8e45
Laimonas Lipinskas <l.lipinskas@student.tudelft.nl>
2022-03-04 18:53:03 +0200

    Added single player controller
    Also changed some fonts in css, tidied up some fmxl files. You can change anything you like.

--- client/src/main/java/client/scenes/SinglePlayerCtrl.java
+++ client/src/main/java/client/scenes/SinglePlayerCtrl.java
@@ -5,11 +5,10 @@ import client.MyModule;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
-import javafx.event.ActionEvent;
 
 import static com.google.inject.Guice.createInjector;
 
-public class NamePromptCtrl {
+public class SinglePlayerCtrl {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
 
@@ -17,7 +16,7 @@ public class NamePromptCtrl {
     private static final MyFXML FXML = new MyFXML(INJECTOR);
 
     @Inject
-    public NamePromptCtrl(ServerUtils server, MainCtrl mainCtrl) {
+    public SinglePlayerCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.server = server;
         this.mainCtrl = mainCtrl;
     }
@@ -26,11 +25,23 @@ public class NamePromptCtrl {
         mainCtrl.showHome();
     }
 
-    public void showGame() {
-        mainCtrl.showGame();
+    public void answerClick() {
+
     }
 
     public void toggleSound() {
+
+    }
+
+    public void hintClick() {
+
+    }
+
+    public void pointsClick() {
+
+    }
+
+    public void timeClick() {
 
     }
 }

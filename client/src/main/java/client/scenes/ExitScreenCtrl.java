8cb7a5f5caae4536bd7ce3fafc5d3a528e046dcd
Reinier Schep <R.J.H.Schep@student.tudelft.nl>
2022-03-05 18:31:14 +0100

    quit functionality + css style
    text inside labels and buttons is outlined

--- client/src/main/java/client/scenes/ExitScreenCtrl.java
+++ client/src/main/java/client/scenes/ExitScreenCtrl.java
@@ -5,6 +5,10 @@ import client.MyModule;
 import client.utils.ServerUtils;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import static com.google.inject.Guice.createInjector;
 
@@ -16,10 +20,25 @@ public class ExitScreenCtrl {
     private static final Injector INJECTOR = createInjector(new MyModule());
     private static final MyFXML FXML = new MyFXML(INJECTOR);
 
+    @FXML
+    Button closeButton;
+    @FXML
+    Button quitButton;
 
     @Inject
     public ExitScreenCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.server = server;
         this.mainCtrl = mainCtrl;
+    }
+
+    public void quit() {
+        Platform.exit();
+    }
+
+    public void closeButtonAction(){
+        // get a handle to the stage
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        // do what you have to do
+        stage.close();
     }
 }

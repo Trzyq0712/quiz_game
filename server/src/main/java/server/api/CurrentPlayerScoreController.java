b3ba33b6845562c811ad91f198fa074b791e039f
Mana Mahmoudi <m.mahmoudi-1@student.tudelft.nl>
2022-03-12 11:12:00 +0100

    Pipeline fixed
    Checkstyle, unused import

--- server/src/main/java/server/api/CurrentPlayerScoreController.java
+++ server/src/main/java/server/api/CurrentPlayerScoreController.java
@@ -1,6 +1,6 @@
 package server.api;
 
-import org.springframework.data.domain.Sort;
+//import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 

1bad17cb5e1acc09faa959b4ea067a8fe9279163
Vian Robotin <V.Robotin@student.tudelft.nl>
2022-03-23 16:03:45 +0100

    Added a new type of question: the player gets an activity and 3 choices for the energy consumption for that activity

---
+++ client/src/main/java/client/scenes/MCQuestionCtrl.java
@@ -0,0 +1,172 @@
+package client.scenes;
+
+import client.utils.ApplicationUtils;
+import client.utils.ServerUtils;
+import commons.Activity;
+import commons.Answer;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+import javax.inject.Inject;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static client.Config.timePerQuestion;
+
+public class MCQuestionCtrl extends BaseCtrl{
+
+    protected final ServerUtils server;
+
+    private Activity activity;
+    private int answerButtonId;
+
+    @FXML
+    ImageView hintJoker;
+    @FXML
+    ImageView pointsJoker;
+    @FXML
+    ImageView timeJoker;
+    @FXML
+    Label ActivityDescription;
+    @FXML
+    ImageView questionImage;
+    @FXML
+    Button firstButton;
+    @FXML
+    Button secondButton;
+    @FXML
+    Button thirdButton;
+    @FXML
+    ProgressBar pgBar;
+    @FXML
+    Label questionTracker;
+    @FXML
+    Label scoreLabel;
+    @FXML
+    VBox chatbox;
+    @FXML
+    StackPane chatAndEmoteHolder;
+
+    @Inject
+    public MCQuestionCtrl(ServerUtils server, MainCtrl mainCtrl, ApplicationUtils utils) {
+        super(mainCtrl, utils);
+        this.server = server;
+    }
+
+    public void pointsClick() {
+        mainCtrl.buttonSound();
+        pointsJoker.setVisible(false);
+    }
+
+    public void emote(Event e){
+        mainCtrl.emote(e);
+    }
+
+    public void timeClick() {
+        mainCtrl.buttonSound();
+        timeJoker.setVisible(false);
+    }
+
+    public void showHome() {
+        mainCtrl.showHome();
+        restoreAnswers();
+        restoreJokers();
+    }
+
+    public void updateTracker() {
+        mainCtrl.updateTracker(questionTracker, scoreLabel, true);
+    }
+
+    public void activateProgressBar() {
+        mainCtrl.activateGenericProgressBar(pgBar, timePerQuestion, 0);
+    }
+
+    public void generateActivity() {
+        activity = server.getActivity();
+        long answer = activity.getEnergyConsumption();
+        Activity a = new Activity(activity.getDescription(), (long)(answer*1.5),activity.getPicturePath());
+        Activity b = new Activity(activity.getDescription(), (long)(answer*0.5),activity.getPicturePath());
+        List<Activity> activities = Arrays.asList(activity,a,b);
+        answerButtonId = 1;
+        displayActivity(activities);
+    }
+
+    private void displayActivity(List<Activity> activities) {
+        ActivityDescription.setText(activity.getDescription());
+        questionImage.setImage(new Image(ServerUtils.SERVER + activity.getPicturePath()));
+        firstButton.setText(String.valueOf(activities.get(0).getEnergyConsumption()));
+        secondButton.setText(String.valueOf(activities.get(1).getEnergyConsumption()));
+        thirdButton.setText(String.valueOf(activities.get(2).getEnergyConsumption()));
+        mainCtrl.setAnswersforAnswerReveal(activities, answerButtonId);
+    }
+
+    /**
+     * hides all buttons except for the one that was clicked
+     * @param event button that was clicked, so either A, B or C
+     */
+    public void answerClick(Event event) {
+        mainCtrl.buttonSound();
+        long timeToAnswer = mainCtrl.getDelta();
+        List<Button> listOfButtons = Arrays.asList(firstButton, secondButton, thirdButton);
+        Button activated = (Button) event.getSource();
+        long i = 0;
+        long buttonNb = 0;
+        for (Button b : listOfButtons) {
+            i++;
+            if (b.getId() != activated.getId()) {
+                b.setVisible(false);
+            } else{
+                buttonNb=i;
+            }
+        }
+        grantPoints(new Answer(buttonNb, timeToAnswer));
+    }
+
+    /**
+     * @param answer - answer the player submitted
+     */
+    public void grantPoints(Answer answer){
+        int earnedPoints = 0;
+        if(answer.getAnswer() == answerButtonId)
+            earnedPoints = answer.getPoints();
+        mainCtrl.getPlayerScore().addPoints(earnedPoints);
+        mainCtrl.setAnswersforAnswerReveal(earnedPoints,false);
+    }
+
+    public void restoreJokers() {
+        hintJoker.setVisible(true);
+        timeJoker.setVisible(true);
+        pointsJoker.setVisible(true);
+    }
+
+    public void restoreAnswers() {
+        firstButton.setVisible(true);
+        secondButton.setVisible(true);
+        thirdButton.setVisible(true);
+    }
+
+    public void hintClick() {
+        mainCtrl.buttonSound();
+        hintJoker.setVisible(false);
+        String falseAnswer = server.activateHint();
+        switch (falseAnswer) {
+            case "a":
+                firstButton.setVisible(false);
+                break;
+            case "b":
+                secondButton.setVisible(false);
+                break;
+            case "c":
+                thirdButton.setVisible(false);
+                break;
+        }
+    }
+}
--- client/src/main/java/client/scenes/MainCtrl.java
+++ client/src/main/java/client/scenes/MainCtrl.java
@@ -87,6 +87,9 @@ public class MainCtrl  {
     private EstimateQuestionCtrl estimateQuestionCtrl;
     private Scene estimateQuestionScene;
 
+    private MCQuestionCtrl MCQuestionCtrl;
+    private Scene MCQuestionScene;
+
     private EditActivityCtrl editActivityCtrl;
     private Scene editActivityScene;
 
@@ -121,7 +124,8 @@ public class MainCtrl  {
                            Pair<PromptCtrl, Parent> prompt,
                            Pair<QuestionCtrl, Parent> question,
                            Pair<EditActivityCtrl, Parent> editActivity,
-                           Pair<EstimateQuestionCtrl, Parent> estimateQuestion) {
+                           Pair<EstimateQuestionCtrl, Parent> estimateQuestion,
+                           Pair<MCQuestionCtrl, Parent> MCQuestion) {
         this.primaryStage = primaryStage;
         /*this.overviewCtrl = overview.getKey();
         this.overview = new Scene(overview.getValue());
@@ -165,6 +169,9 @@ public class MainCtrl  {
 
         this.estimateQuestionCtrl = estimateQuestion.getKey();
         this.estimateQuestionScene = new Scene(estimateQuestion.getValue());
+
+        this.MCQuestionCtrl = MCQuestion.getKey();
+        this.MCQuestionScene = new Scene(MCQuestion.getValue());
 
         this.editActivityCtrl = editActivity.getKey();
         this.editActivityScene = new Scene(editActivity.getValue());
@@ -211,7 +218,7 @@ public class MainCtrl  {
     public void initializeHolders() {
         listOfHolders = Arrays.asList(questionCtrl.chatAndEmoteHolder, answerRevealCtrl.chatAndEmoteHolder,
                 intermediateCtrl.chatAndEmoteHolder, MPFinal.chatAndEmoteHolder,
-                 estimateQuestionCtrl.chatAndEmoteHolder);
+                 estimateQuestionCtrl.chatAndEmoteHolder, MCQuestionCtrl.chatAndEmoteHolder);
     }
 
     /**
@@ -222,6 +229,7 @@ public class MainCtrl  {
     public void activateSingleplayer() {
         questionCtrl.timeJoker.setVisible(false);
         estimateQuestionCtrl.timeJoker.setVisible(false);
+        MCQuestionCtrl.timeJoker.setVisible(false);
         for (StackPane s : listOfHolders) {
             s.setVisible(false);
         }
@@ -327,8 +335,8 @@ public class MainCtrl  {
             activateSingleplayer();
         else
             activateMultiplayer();
-        int value = (int)(Math.random()*2);
-        switch (value%2){
+        int value = (int)(Math.random()*3);
+        switch (value%3){
             case 0: {
                 questionScene.getStylesheets().add(Config.styleSheet);
                 questionCtrl.updateTracker();
@@ -343,6 +351,15 @@ public class MainCtrl  {
                 estimateQuestionCtrl.generateActivity();
                 primaryStage.setScene(estimateQuestionScene);
                 new Thread(() -> estimateQuestionCtrl.activateProgressBar()).start();
+                break;
+            }
+
+            case 2: {
+                MCQuestionScene.getStylesheets().add(Config.styleSheet);
+                MCQuestionCtrl.updateTracker();
+                MCQuestionCtrl.generateActivity();
+                primaryStage.setScene(MCQuestionScene);
+                new Thread(() -> MCQuestionCtrl.activateProgressBar()).start();
                 break;
             }
         }
@@ -404,6 +421,7 @@ public class MainCtrl  {
                     Platform.runLater(() -> showAnswerReveal());
                 } else if (call == 1 && currentQuestion < Config.totalQuestions) {
                     questionCtrl.restoreAnswers();
+                    MCQuestionCtrl.restoreAnswers();
                     if (singlePlayerModeActive) Platform.runLater(() -> showQuestion());
                     else Platform.runLater(() -> showIntermediateLeaderboard());
                 } else if (call == 1 && currentQuestion >= Config.totalQuestions) {
@@ -433,7 +451,9 @@ public class MainCtrl  {
         currentQuestion = 0;
         questionCtrl.restoreJokers();
         estimateQuestionCtrl.restoreJokers();
+        MCQuestionCtrl.restoreJokers();
         questionCtrl.restoreAnswers();
+        MCQuestionCtrl.restoreAnswers();
     }
 
     /**

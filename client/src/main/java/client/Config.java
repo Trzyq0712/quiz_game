8416c9529e10051e475c5a588800085dbba7598a
Vian Robotin <V.Robotin@student.tudelft.nl>
2022-03-25 17:38:13 +0100

    Gradle run works now

--- client/src/main/java/client/Config.java
+++ client/src/main/java/client/Config.java
@@ -3,8 +3,8 @@ package client;
 import java.io.File;
 
 public class Config {
-    public static File backgroundMusic = new File("client/src/main/resources/music.mp3");
-    public static File buttonClickSound = new File("client/src/main/resources/button2.mp3");
+    public static File backgroundMusic = new File("src/main/resources/music.mp3");
+    public static File buttonClickSound = new File("src/main/resources/button2.mp3");
     public static String styleSheet = "style.css";
     public static String title = "The Energy Quiz";
     public static String quit = "Sure you want to quit?";

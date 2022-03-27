29dbfeec3d354ce1de7ae8b4790846d9060b16f5
Reinier Schep <R.J.H.Schep@student.tudelft.nl>
2022-03-27 19:20:17 +0200

    fix pipeline again

--- server/src/test/java/server/api/PreGameControllerTest.java
+++ server/src/test/java/server/api/PreGameControllerTest.java
@@ -1,7 +1,6 @@
 package server.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import commons.Player;
 import commons.PlayerScore;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;

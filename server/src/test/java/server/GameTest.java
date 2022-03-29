6e5404fc83652389e7ad1c2cc0b11df34d79a8bd
Reinier Schep <R.J.H.Schep@student.tudelft.nl>
2022-03-29 19:36:19 +0200

    removed unused import

--- server/src/test/java/server/GameTest.java
+++ server/src/test/java/server/GameTest.java
@@ -4,10 +4,6 @@ import commons.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {

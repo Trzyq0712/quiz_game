3cd081e595141cdad118c721c1fc562e5ef4a45d
Kuba Trzykowski <J.A.Trzykowski@student.tudelft.nl>
2022-03-31 19:24:57 +0200

    Try fixing the progress bar

--- client/src/main/java/client/utils/ApplicationUtils.java
+++ client/src/main/java/client/utils/ApplicationUtils.java
@@ -1,19 +1,15 @@
 package client.utils;
 
 
-import static commons.Config.*;
-import static client.utils.Config.*;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.control.ProgressBar;
 import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -24,6 +20,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import static client.utils.Config.*;
+import static commons.Config.maxAmountOfNotifications;
 
 
 /**
@@ -56,40 +55,35 @@ public class ApplicationUtils {
      * After the time runs out the callback function is executed.
      *
      * @param progressBar The progress bar that is to be modified.
-     * @param runTime Time given in milliseconds. Defines how long the progress bar should run for.
-     * @param callback The function to be executed after the progress bar runs out i.e. the runTime time passes.
+     * @param runTime     Time given in milliseconds. Defines how long the progress bar should run for.
+     * @param callback    The function to be executed after the progress bar runs out i.e. the runTime time passes.
      */
     public void runProgressBar(ProgressBar progressBar, long runTime, Runnable callback) {
         this.startTime = System.currentTimeMillis();
         this.runTime = runTime;
         this.reducedTime = 0;
         progress = new SimpleDoubleProperty(1.0);
-        progress.lessThanOrEqualTo(0.0).addListener(new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-                System.out.println("Calling the callback");
-                Platform.runLater(callback::run);
-                cancelProgressBar();
-            }
-        });
-        for (var p : colorUpdates) {
-            progress.lessThanOrEqualTo(p.getKey()).addListener(new ChangeListener<Boolean>() {
-                @Override
-                public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-                    Platform.runLater(() -> progressBar.setStyle("-fx-accent: " + p.getValue()));
-                }
-            });
-        }
         progressBar.progressProperty().bind(progress);
+        progress.lessThanOrEqualTo(0.0)
+                .addListener((observable, oldValue, newValue) -> {
+                    System.out.println("Calling the callback");
+                    Platform.runLater(callback::run);
+                    cancelProgressBar();
+                });
+        for (var p : colorUpdates) {
+            progress.lessThanOrEqualTo(p.getKey())
+                    .addListener((observable, oldValue, newValue) ->
+                            Platform.runLater(() -> progressBar.setStyle("-fx-accent: " + p.getValue())));
+        }
         timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 long timePassed = System.currentTimeMillis() - startTime + reducedTime;
-                Platform.runLater(() -> progress.set(1.0 - (double)timePassed / (double)runTime));
+                Platform.runLater(() -> progress.set(1.0 - (double) timePassed / (double) runTime));
                 System.out.println(progress.getValue());
             }
-        }, 10, 50);
+        }, 10, 100);
     }
 
     /**
@@ -102,11 +96,12 @@ public class ApplicationUtils {
 
     /**
      * Reduce the progress bar runtime by adding to the total reduced time.
+     *
      * @param reductionFactor The reduction factor i.e. 0.2 would mean the remaining time is reduced by 20%.
      */
     public void reduceTime(double reductionFactor) {
         long remainingTime = runTime - (System.currentTimeMillis() - startTime);
-        reducedTime += (long)((double)remainingTime * reductionFactor);
+        reducedTime += (long) ((double) remainingTime * reductionFactor);
     }
 
     /**
@@ -124,6 +119,7 @@ public class ApplicationUtils {
 
     /**
      * Register a music indicators for changes when sound is being toggled.
+     *
      * @param iv The ImageView which should be registered for changes. The image will change between an on and off.
      */
     public void registerMusicToggle(ImageView iv) {
@@ -132,6 +128,7 @@ public class ApplicationUtils {
 
     /**
      * Adds the notification box of the inheriting controller to the list of all notification boxes.
+     *
      * @param notificationBox Notification box to be registered, so notifications can be added later.
      */
 

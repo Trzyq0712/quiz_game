8038fbbb119d15c83c8c66a4cf6d2474deb2ead2
Laimonas Lipinskas <l.lipinskas@student.tudelft.nl>
2022-04-05 18:03:46 +0300

    mini change

--- commons/src/main/java/commons/Config.java
+++ commons/src/main/java/commons/Config.java
@@ -8,11 +8,10 @@ public class Config {
     public static String titleWaitingRoom = "The Waiting Room";
     public static long timePerQuestion = 15000; //milliseconds
     public static long timeForIntermediate = 3000; //milliseconds, time the intermediate leaderboard shows
-    public static long timeAnswerReveal = 5000; //milliseconds, time the revealed answers show\
+    public static long timeAnswerReveal = 3000; //milliseconds, time the revealed answers show\
     public static int totalQuestions = 2; //total amount of questions in the quiz, set to 2 for testing purposes
     public static String edit = "Edit activities";
     public static int maxChatMessages = 5;//maximum amount of chatmessages that can be in the chat at once
-    public static String serverImagePath = "server\\src\\main\\resources\\static\\activity\\newActivities\\";
     public static int maxCharsUsername = 20; //maximum amount of characters a name can contain
     public static String port = "8080"; //port for server and client
     public static String server = "http://localhost:" + port + "/";

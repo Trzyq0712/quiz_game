27effd6443a205190b9624ec865c5fa6fab5469a
Kuba Trzykowski <J.A.Trzykowski@student.tudelft.nl>
2022-04-05 16:25:30 +0200

    Change emoji fetching

--- client/src/main/java/client/scenes/MainCtrl.java
+++ client/src/main/java/client/scenes/MainCtrl.java
@@ -396,7 +396,8 @@ public class MainCtrl {
         for (VBox c : listOfChatBoxes) {
             Platform.runLater(() -> {
                 HBox hbox = new HBox();
-                Image arg = new Image(ServerUtils.SERVER + "emotes" + path);
+//                Image arg = new Image(ServerUtils.SERVER + "emotes" + path);
+                Image arg = new Image(getClass().getClassLoader().getResource("images" + path).toExternalForm());
                 Label user = new Label(name + ":  ");
                 ImageView emote = new ImageView(arg);
                 emote.setFitHeight(50);

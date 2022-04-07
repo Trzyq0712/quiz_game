5e794f717d6d5f0936e6eb2cba770a55d21ba26b
Vian Robotin <V.Robotin@student.tudelft.nl>
2022-04-07 02:00:35 +0200

    Set question time to 10s and 20 questions per game

--- commons/src/main/java/commons/Config.java
+++ commons/src/main/java/commons/Config.java
@@ -6,10 +6,10 @@ public class Config {
     public static String title = "The Energy Quiz";
     public static String quit = "Sure you want to quit?";
     public static String titleWaitingRoom = "The Waiting Room";
-    public static long timePerQuestion = 15000; //milliseconds
-    public static long timeForIntermediate = 3000; //milliseconds, time the intermediate leaderboard shows
-    public static long timeAnswerReveal = 3000; //milliseconds, time the revealed answers show\
-    public static int totalQuestions = 2; //total amount of questions in the quiz, set to 2 for testing purposes
+    public static long timePerQuestion = 10000; //milliseconds
+    public static long timeForIntermediate = 5000; //milliseconds, time the intermediate leaderboard shows
+    public static long timeAnswerReveal = 5000; //milliseconds, time the revealed answers show\
+    public static int totalQuestions = 20; //total amount of questions in the quiz, set to 2 for testing purposes
     public static String edit = "Edit activities";
     public static int maxChatMessages = 5;//maximum amount of chatmessages that can be in the chat at once
     public static int maxCharsUsername = 20; //maximum amount of characters a name can contain
